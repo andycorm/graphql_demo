ea0eb12b06ddbd2afe07a25b31aabeecad168586
Andy Corman <andycorm@gmail.com>
2019-05-20 17:12:54 -0700

    Added packages/logic to dbConnectors

--- data/schema.js
+++ data/schema.js
@@ -12,6 +12,13 @@ const typeDefs = `
         contacts: [Contact]
     }
 
+    type Alien {
+        id: ID
+        firstName: String
+        lastName: String
+        planet: String
+    }
+
     type Contact {
         firstName: String
         lastName: String
